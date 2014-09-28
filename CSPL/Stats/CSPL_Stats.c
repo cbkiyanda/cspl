28291041125d678fa40dea4316190ee52f95b75e
Brian Larsen <balarsen@lanl.gov>
2014-09-27 21:38:46 -0600

    Stats: added mean and median

---
+++ CSPL/Stats/CSPL_Stats.c
@@ -0,0 +1,54 @@
+
+#include <stdlib.h> /* memcpy */
+#include <string.h> /* memcpy */
+#include "CSPL_Stats.h"
+
+// from stdlib example docs
+static int compare_doubles(const void *a, const void *b)
+{
+  const double *da = (const double *) a;
+  const double *db = (const double *) b;
+
+  return (*da > *db) - (*da < *db);
+}
+
+
+double CSPL_mean(double *inval, long n) {
+  double sum=0;
+  long i;
+
+  for (i=0;i<n;i++)
+    sum += inval[i];
+  return (sum/(double)n);
+}
+
+double CSPL_meadian(double *inval, long n) {
+  double *incopy; // do this so that the input array is not changed
+  double median; 
+  long index;
+  double tmpvals[2]; // for the even case
+
+  incopy = (double*)calloc(n, sizeof(double));
+  memcpy(incopy, inval, sizeof(double)*n);
+  
+  // sort the data
+  qsort(incopy, n, sizeof(double *), compare_doubles);
+  if ( 0 == n % 2) {
+    index = (n/2)-1;
+    tmpvals[0] = incopy[index];
+    tmpvals[1] = incopy[index+1];
+    median = CSPL_mean(incopy, 2);
+  } else {
+    // median is the n/2th item
+    median = incopy[((n+1)/2)-1];
+  }
+  free(incopy);  
+  return(median);
+}
+
+
+
+
+
+
+
--- CSPL/Stats/CSPL_Stats.h
+++ CSPL/Stats/CSPL_Stats.h
@@ -1,10 +1,12 @@
 #ifndef CSPL_Stats_H
 #define CSPL_Stats_H
  
+double CSPL_mean(double *inval,    // input array 
+		 long n);          // length of array
+double CSPL_meadian(double *inval, // input array
+		    long n);       // length of array
 
 
-
- 
 #endif /* CSPL_Stats_H */
 
 
