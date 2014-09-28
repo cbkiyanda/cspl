987bf3579ad58fbaa145fe01c904e875aa0abefa
Brian Larsen <balarsen@lanl.gov>
2014-09-27 21:40:19 -0600

    examples: descriptive.c creates for stats, can't get it to compile, missing lib

---
+++ Examples/Stats/descriptive.c
@@ -0,0 +1,32 @@
+
+// gcc -I../../CSPL/Stats -L../../ descriptive.c -lCSPL -o descriptive
+
+#include <stdio.h>
+
+#include "CSPL_Stats_CDF.h"
+#include "CSPL_Stats.h"
+
+int main(void) {
+  double dat1[5] = {1, 2, 5, 7, 8};
+  double dat2[6] = {1, 2, 2, 6, 7, 8};
+  long i;
+  
+  for (i=0;i<5;i++) 
+    printf("dat1[%ld]=%lf\n", i, dat1[i]);
+  printf("\n");
+  for (i=0;i<6;i++) 
+    printf("dat2[%ld]=%lf\n", i, dat1[i]);
+  
+  printf("mean1: %lf\n", CSPL_mean(dat1, 5));
+  printf("mean2: %lf\n", CSPL_mean(dat2, 6));
+
+
+
+  return(0);
+}
+
+
+
+
+
+
