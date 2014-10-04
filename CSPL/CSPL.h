a4668792310a9e7e26d389619fd014c08d0af028
Brian Larsen <balarsen@lanl.gov>
2014-10-04 11:26:16 -0600

    CSPL.h: is this how we want to handle overall constants? Good for now

---
+++ CSPL/CSPL.h
@@ -0,0 +1,17 @@
+#ifndef CSPL_H
+#define CSPL_H
+
+#ifndef CSPL_Sign
+#define CSPL_Sign(x) (((x) > 0) ? 1 : ((x < 0) ? -1 : 0))
+#endif
+
+#ifndef CSPL_Pi
+#define CSPL_Pi 3.14159265359
+#endif 
+
+
+
+#endif /* CSPL_H */
+
+
+
