bd45e0c9acfa4acaf2c5d013d1c9f255db4710c7
Brian Larsen <balarsen@lanl.gov>
2014-10-04 10:41:40 -0600

    Special: setup a default Erf(), there is probably a better way to do this

--- CSPL/Special/CSPL_Special.h
+++ CSPL/Special/CSPL_Special.h
@@ -5,6 +5,12 @@
 #define CSPL_Sign(x) (((x) > 0) ? 1 : ((x < 0) ? -1 : 0))
 #endif
 
+/* setup the defualt Erf */
+#ifndef CSPL_Special_Erf
+#define CSPL_Special_Erf CSPL_Special_Erf1
+#endif
+
+
 /*
  * Compute the error function with a maximal error of  1.5×10−7
  * Abramowitz, Milton, and Irene A. Stegun, eds. Handbook of mathematical functions: with formulas, graphs, 
