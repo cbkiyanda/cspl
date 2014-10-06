d47a0becc4e27dda05b7610ad6ff7c6e348805a5
Brian Larsen <balarsen@lanl.gov>
2014-10-05 19:33:20 -0600

    Stats: checks all the tests from Stats.check to Test_Stats.c

--- Tests/Test_Stats.c
+++ Tests/Test_Stats.c
@@ -20,6 +20,7 @@ int main(void) {
   double median_ans   =  34.000000;
   double std_ans      =  16.078321;
   double variance_ans = 258.512397;
+  double geo_ans      = 23.449640;
 
   ans = CSPL_Stats_skew(data1, 11);
   if (float_test(ans, skew_ans, 1e-4) == 0) {
@@ -56,6 +57,71 @@ int main(void) {
     printf("CSPL_Stats_variance=%lf, %lf\n", ans, variance_ans);
     return(1);
   }
+
+  ans = CSPL_Stats_geometric_mean(data1, 11);
+  if (float_test(ans, geo_ans, 1e-4) == 0) {
+    printf("CSPL_Stats_geometric_mean=%lf, %lf\n", ans, geo_ans);
+    return(1);
+  }
+
+  {
+    double data[5] = {2,3.4, 5.6, 4.5, 3.1};
+    double outval[5];
+    double answer[5] = {0.2, 0.4, 0.6, 0.8, 1.0};
+    long i;
+    CSPL_Stats_tCDF(data, outval, 5);
+    for (i=0;i<5;i++) {
+      if( float_test(outval[i], answer[i], 1e-5)==0) return(1);
+    }
+  }
+  {
+    double data[5] =  {0.2, 0.4, 0.6, 0.8, 1.0};
+    double outval1[5];
+    double outval2[5];
+    double answer1[5] = {0.0, 0.0, 0.0, 0.072105, 0.272105};
+    double answer2[5] = {0.927895, 1.0, 1.0, 1.0, 1.0};
+    long i;
+    CSPL_Stats_tCDF_confidence_bands(data, outval1, outval2, 0.01, 5);
+    /* for (i=0;i<5;i++) {
+    // printf("%lf\t%lf\n", outval1[i], outval2[i]);
+    } */
+    for (i=0;i<5;i++) {
+      if( float_test(outval1[i], answer1[i], 1e-5)==0)return(1);
+      if( float_test(outval2[i], answer2[i], 1e-5)==0)return(1);
+    }	  
+  }
+  {
+    double data[5] =  {0.2, 0.4, 0.6, 0.8, 1.0};
+    printf("CSPL_Stats_percentile %lf\n", CSPL_Stats_percentile(data, 20, 5));
+    if( float_test(CSPL_Stats_percentile(data, 20, 5), 0.300000, 1e-5) ==0)return(1);
+    printf("CSPL_Stats_percentile %lf\n", CSPL_Stats_percentile(data, 25, 5));
+    if( float_test(CSPL_Stats_percentile(data, 25, 5), 0.350000, 1e-5) ==0)return(1);
+    printf("CSPL_Stats_percentile %lf\n", CSPL_Stats_percentile(data, 40, 5));
+    if( float_test(CSPL_Stats_percentile(data, 40, 5), 0.500000, 1e-5) ==0)return(1);
+    printf("CSPL_Stats_percentile %lf\n", CSPL_Stats_percentile(data, 50, 5));
+    if( float_test(CSPL_Stats_percentile(data, 50, 5), 0.600000, 1e-5) ==0)return(1);
+    printf("CSPL_Stats_percentile %lf\n", CSPL_Stats_percentile(data, 60, 5));
+    if( float_test(CSPL_Stats_percentile(data, 60, 5), 0.700000, 1e-5) ==0)return(1);
+    printf("CSPL_Stats_percentile %lf\n", CSPL_Stats_percentile(data, 82, 5));
+    if( float_test(CSPL_Stats_percentile(data, 82, 5), 0.920000, 1e-5) ==0)return(1);
+    if( float_test(CSPL_Stats_percentile(data, 50, 5), CSPL_Stats_median(data, 5) , 1e-5) ==0)return(1);
+  }
+  {
+    long N=11;
+    long n;
+    double answer[11] = {4.545455, 13.636364, 22.727273, 31.818182, 40.909091, 
+			 50.000000, 59.090909, 68.181818, 77.272727, 86.363636, 
+			 95.454545};
+    for (n=0;n<N;n++) {
+      printf("CSPL_Stats_percent_rank %ld, %ld, %lf\n", n, N, CSPL_Stats_percent_rank(n, N));
+      if( float_test(CSPL_Stats_percent_rank(n, N), answer[n] , 1e-5)==0 )return(1);
+    }	
+  }
+
+
+
+  
+
 
   return(0);
 }
