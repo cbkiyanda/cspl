a9e995266b701a1bf14d6bddac01871bac3f5cb4
Brian Larsen <balarsen@lanl.gov>
2014-10-05 19:32:34 -0600

    Array: and add the test file to the repo, oops

---
+++ Tests/Test_Array.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+
+#include "CSPL_Test.h"
+#include "../CSPL/Array/CSPL_Array.h"
+
+
+int main(void) {
+  {
+    double d1[5] = {2,3.4, 5.6, 4.5, 3.1};
+    double d2[5] = {4.5, 2.3, 3.4, 1.0, 4.5};
+    double answer[5] = {6.5, 5.7, 9.0, 5.5, 7.6};
+    double out[5];
+    long i;
+    CSPL_Array_add(d1, d2, out, 5);
+    for (i=0;i<5;i++) {
+      printf("CSPL_Array_add[%ld]=%lf  %lf\n", i, out[i], answer[i]);
+      if ( float_test(out[i], answer[i], 1e-5) == 0) {
+	return (1);
+      }
+    }
+
+  }
+  {
+    double d1[5] = {2,3.4, 5.6, 4.5, 3.1};
+    double d2[5] = {4.5, 2.3, 3.4, 1.0, 4.5};
+    double answer[5] = {-2.5, 1.1, 2.2, 3.5, -1.4};
+    double out[5];
+    long i;
+    CSPL_Array_subtract(d1, d2, out, 5);
+    for (i=0;i<5;i++) {
+      printf("CSPL_Array_subtract[%ld]=%lf  %lf\n", i, out[i], answer[i]);
+      if( float_test(out[i], answer[i], 1e-5) == 0) {
+	return(1);
+      }
+    }
+  }
+  {
+    double d1[5] = {2,   3.4, 5.6, 4.5, 3.1};
+    double d2[5] = {4.5, 2.3, 3.4, 1.0, 4.5};
+    double answer[5] = {9.0, 7.82, 19.04, 4.5, 13.95};
+    double out[5];
+    long i;
+    CSPL_Array_multiply(d1, d2, out, 5);
+    for (i=0;i<5;i++) {
+      printf("CSPL_Array_multiply[%ld]=%lf  %lf\n", i, out[i], answer[i]);
+      if( float_test(out[i], answer[i], 1e-5) == 0) {
+	return(1);
+      }
+    }
+  }
+  {
+    double d1[5] = {2,   3.4, 5.6, 4.5, 3.1};
+    double d2[5] = {4.5, 2.3, 3.4, 1.0, 4.5};
+    double answer[5] = {0.444444, 1.478261, 1.647059, 4.5, 0.688889};
+    double out[5];
+    long i;
+    CSPL_Array_divide(d1, d2, out, 5);
+    for (i=0;i<5;i++) {
+      printf("CSPL_Array_divide[%ld]=%lf  %lf\n", i, out[i], answer[i]);
+      if( float_test(out[i], answer[i], 1e-5)==0) {
+	return(1);
+      }
+    }
+  }
+  {
+    double d1[5] = {2,   3.4, 5.6, 4.5, 3.1};
+    double answer = 2.0;
+    double out;	     
+    out = CSPL_Array_min(d1, 5);
+    if( float_test(out, answer, 1e-5)==0) {
+      return(1);
+    }
+  }
+  {
+    double d1[5] = {2,   3.4, 5.6, 4.5, 3.1};
+    double answer = 5.6;
+    double out;	     
+    out = CSPL_Array_max(d1, 5);
+    if( float_test(out, answer, 1e-5)==0) {
+      return(1);
+    }
+  }
+  {
+    double d1[5] = {2,   3.4, 5.6, 4.5, 3.1};
+    double answer = 3.6;
+    double out;	     
+    out = CSPL_Array_range(d1, 5);
+    if( float_test(out, answer, 1e-5)==0) {
+      return(1);
+    }
+  }
+  {
+    double d1[5] = {2,   3.4, 5.6, 4.5, 3.1};
+    double answer1[5] = {2,3.4,5.6, 4.5, 3.1};
+    double answer2[5] = {3,3.4,5.6, 4.5, 3.1};
+    double answer3[5] = {3,3.4,5.5, 4.5, 3.1};
+    double answer4[5] = {-4, -4, -4, -4, -4};
+    long i;
+    CSPL_Array_clip(d1, 0, 10, 5);
+    for (i=0;i<5;i++) {
+      printf("CSPL_Array_clip[%ld]=%lf  %lf\n", i, d1[i], answer1[i]);
+      if( float_test(d1[i], answer1[i], 1e-5)==0) return(1);
+    }
+    CSPL_Array_clip(d1, 3, 10, 5);
+    for (i=0;i<5;i++) {
+      printf("CSPL_Array_clip[%ld]=%lf  %lf\n", i, d1[i], answer2[i]);
+      if( float_test(d1[i], answer2[i], 1e-5)==0) return(1);
+    }
+    CSPL_Array_clip(d1, 3, 5.5, 5);
+    for (i=0;i<5;i++) {
+      printf("CSPL_Array_clip[%ld]=%lf  %lf\n", i, d1[i], answer3[i]);
+      if( float_test(d1[i], answer3[i], 1e-5)==0) return(1);
+    }
+    CSPL_Array_clip(d1, -10, -4, 5);
+    for (i=0;i<5;i++) {
+      printf("CSPL_Array_clip[%ld]=%lf  %lf\n", i, d1[i], answer4[i]);
+      if( float_test(d1[i], answer4[i], 1e-5)==0) return(1);
+    }  
+  }
+  
+
+
+
+
+  return(0);
+}
+
